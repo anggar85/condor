4ec1998dc97d47bb4294363201b8f591d748ca5f
Angel Canales <anggar85@gmail.com>
2022-02-13 01:15:45 -0800

    chore: add comments and order to all methods

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const SAMPLE_TEXT =  ["greet",
 // Main LISTS
 let dataList1 = [];
 let dataList2 = [];
-let sorts = [1,1];
+let sorts     = [1,1];
 
 // I'm populating dataList1 with sample data
 for (let x = 0; x < SAMPLE_TEXT.length; x++) {
@@ -26,15 +26,13 @@ for (let x = 0; x < SAMPLE_TEXT.length; x++) {
 
 // ELEMENTS
 
-const divList = document.getElementById("list1");
-const divList2 = document.getElementById("list2");
-
-
-const searchL1 = document.getElementById("search1");
-const searchL2 = document.getElementById("search2");
-const addNew = document.getElementById("addNew");
-const btnCancelNewItem = document.getElementById("btnCancelNewItem");
-const btnSaveNewItem = document.getElementById("btnSaveNewItem");
+const divList       = document.getElementById("list1");
+const divList2      = document.getElementById("list2");
+const searchL1      = document.getElementById("search1");
+const searchL2      = document.getElementById("search2");
+const addNew        = document.getElementById("addNew");
+const btnCancelNewItem  = document.getElementById("btnCancelNewItem");
+const btnSaveNewItem    = document.getElementById("btnSaveNewItem");
 
 
 // LISTENERS
@@ -46,64 +44,14 @@ btnSaveNewItem.addEventListener("click", saveNewItem);
 
 
 
-
-// METHODS
-
-function deleteItem(index) {
-  delete dataList1[index]
-  dataList1 = dataList1.filter( d => d);
-  fillList1();
-}
-
-function deleteItemFromList2(index) {
-  delete dataList2[index]
-  dataList2 = dataList2.filter( d => d);
-  fillList2();
-}
-
-function saveNewItem() {
-  const inputNewRow = document.getElementById("inputNewRow");
-  let obj = {title: inputNewRow.value, status: PENDING, show: true};
-  dataList1.push(obj);
-  fillList1();
-  cancelNewItem();
-}
+//////////////////////
+// FUNCTIONS
+//////////////////////
 
 
-function cancelNewItem() {
-  const list1NewRow = document.getElementById("list1NewRow");
-  list1NewRow.classList.add("invisibleCustom");
-  resetInput();
-}
+// LIST1 functions
 
-
-function addNewRow() {
-  const list1NewRow = document.getElementById("list1NewRow");
-  list1NewRow.classList.remove("invisibleCustom");
-  document.getElementById("inputNewRow").focus();
-}
-
-function resetInput() {
-  const inputNewRow = document.getElementById("inputNewRow");
-  inputNewRow.value = "";
-}
-
-
-function searchTasks(value, dataSet, callBack) {
-  if (value === "")
-    dataSet.forEach((d, i) => {
-      dataSet[i].show = true
-    })
-
-  dataSet.forEach((d, i) => {
-    if (!d.title.toLowerCase().includes(value.toLowerCase()))
-      dataSet[i].show = false 
-  });
-
-  callBack();
-    
-}
-
+// Inject the list of pending tasks
 function fillList1() {
   dataList1 = dataList1.sort((a,b)=> sortData(a,b ))
   let info = "";
@@ -141,7 +89,66 @@ function fillList1() {
   }
 }
 
+// Change UI and replace static text with an input and buttons
+function updateItem(index) {
+  const value = dataList1[index].title
+  const element = document.getElementById("title_" + index)
+  element.innerHTML = `
+                      <div  class="w-100">
+                        <div>
+                          <input id="input_${index}" class="form-control" type="text" value="${value}" />
+                        </div>
+                      </div>
+                      <div class="d-flex align-items-center">
+                        <img data-id="${index}_cancel" src="src/img/cancel.png" class="isCursor miniIcon" />
+                        <img onclick="saveUpdate(${index})" data-id="${index}_save" src="src/img/check.png" class="ml-2 mr-2 isCursor miniIcon" />
+                      </div>`;
+                      updateIconsListeners()
+}
 
+// Save the changes in the item/task
+function saveUpdate(index) {
+  const value = document.getElementById("input_" + index).value;
+  let obj = {title: value, status: PENDING, show: true};
+  dataList1[index] = obj;
+  fillList1();
+}
+
+// Remove a task from the main array
+function deleteItem(index) {
+  delete dataList1[index]
+  dataList1 = dataList1.filter( d => d);
+  fillList1();
+}
+
+// Changes UI to display an input and buttons for the new task
+function addNewRow() {
+  const list1NewRow = document.getElementById("list1NewRow");
+  list1NewRow.classList.remove("invisibleCustom");
+  document.getElementById("inputNewRow").focus();
+}
+
+// Save the new item/task
+function saveNewItem() {
+  const inputNewRow = document.getElementById("inputNewRow");
+  let obj = {title: inputNewRow.value, status: PENDING, show: true};
+  dataList1.push(obj);
+  fillList1();
+  cancelNewItem();
+}
+
+// Abort creation of new item and changes the UI
+function cancelNewItem() {
+  const list1NewRow = document.getElementById("list1NewRow");
+  list1NewRow.classList.add("invisibleCustom");
+  const inputNewRow = document.getElementById("inputNewRow");
+  inputNewRow.value = "";
+}
+
+
+// LIST2 functions
+
+// Inject the list of completed tasks
 function fillList2() {
   dataList2 = dataList2.sort((a,b)=> sortData(a,b ))
   let info = "";
@@ -179,6 +186,19 @@ function fillList2() {
   }
 }
 
+// Remove a task from the completed tasks array
+function deleteItemFromList2(index) {
+  delete dataList2[index]
+  dataList2 = dataList2.filter( d => d);
+  fillList2();
+}
+
+
+/////////////////
+// GENERAL USER
+/////////////////
+
+// Every time a checkbox is selected this function will update both lists
 function changeItemFromList(index, list) {
   if (list === LIST1) {
     dataList1 = [...dataList1, dataList2[index]].filter((d)=> d)
@@ -194,22 +214,30 @@ function changeItemFromList(index, list) {
 }
 
 
-function updateItem(index) {
-  const value = dataList1[index].title
-  const element = document.getElementById("title_" + index)
-  element.innerHTML = `
-                      <div  class="w-100">
-                        <div>
-                          <input id="input_${index}" class="form-control" type="text" value="${value}" />
-                        </div>
-                      </div>
-                      <div class="d-flex align-items-center">
-                        <img data-id="${index}_cancel" src="src/img/cancel.png" class="isCursor miniIcon" />
-                        <img onclick="saveUpdate(${index})" data-id="${index}_save" src="src/img/check.png" class="ml-2 mr-2 isCursor miniIcon" />
-                      </div>`;
-                      updateIconsListeners()
+// This function can be used for both lists to search tasks
+function searchTasks(value, dataSet, callBack) {
+  if (value === "")
+    dataSet.forEach((d, i) => {
+      dataSet[i].show = true
+    })
+
+  dataSet.forEach((d, i) => {
+    if (!d.title.toLowerCase().includes(value.toLowerCase()))
+      dataSet[i].show = false 
+  });
+  callBack();
 }
 
+// Simple sort for the task in asc
+function sortData( a, b ) {
+  if ( a.title < b.title )
+    return -1;
+  if ( a.title > b.title )
+    return 1;
+  return 0;
+}
+
+// Due injecton of HTML I need to reload the listeners
 function updateIconsListeners() {
   const divListImgs = document.querySelectorAll('#list1 img, #list2 img');
     divListImgs.forEach(e => e.addEventListener("click", function(e) {
@@ -231,26 +259,6 @@ function updateIconsListeners() {
           break;
       }
     }));
-}
-
-
-function saveUpdate(index) {
-  const value = document.getElementById("input_" + index).value;
-  let obj = {title: value, status: PENDING, show: true};
-  dataList1[index] = obj;
-  fillList1();
-}
-
-function sortData( a, b ) {
-  if ( a.title < b.title )
-    return -1;
-  if ( a.title > b.title )
-    return 1;
-  return 0;
-}
-
-function changeSortOrder(params) {
-  
 }
 
 (function () {
